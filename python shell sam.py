abbe00a1bb2184ab5adfabaa67f4e1bef402e15b
Samlacasse81 <sammybaby2003.sl@gmail.com>
2017-07-18 15:38:29 -0400

    Add files via upload

---
+++ python shell sam.py
@@ -0,0 +1,71 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+5
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+5
+apple
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+5
+apple
+APPLE
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+5
+apple
+APPLE
+redhat
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+5
+apple
+APPLE
+red hat
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+red hat
+whats your name?sam
+sam
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+red hat
+whats your name? sam
+sam
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+Traceback (most recent call last):
+  File "//labfiles/VentureFolders$/vg10/Desktop/sam81.py", line 70, in <module>
+    value1 = false
+NameError: name 'false' is not defined
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+Traceback (most recent call last):
+  File "//labfiles/VentureFolders$/vg10/Desktop/sam81.py", line 75, in <module>
+    value2 = 100**0.5 >= 50 or false
+NameError: name 'false' is not defined
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+Hello
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+Hello
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+bye
+>>> 
+========= RESTART: //labfiles/VentureFolders$/vg10/Desktop/sam81.py =========
+my name is bob
+>>> 
---
+++ sam81.py
@@ -0,0 +1,121 @@
+number = 2 #assigning number variable the value of 2
+
+"""addition = 50 + 75
+subtraction = 100 - 30
+multiplication = 4 * 4
+division = 80/10
+
+print (addition)
+print (subtraction)
+print (multiplication)
+print (division)"""
+
+"""exponent = 5**7
+module = 15/4 #output of 3"""
+
+"""cost = 50
+tax = 1.13
+tip = 1.15
+
+subtotal= cost +(cost * tax)
+total = subtotal + (subtotal + tip)
+
+total = cost * tax * tip
+total = 50 * 1.13 * 1.15
+total = 64.975
+print (total)"""
+
+"""name = "bob"
+print (name[1])"""
+
+"""fruit = "apple"
+print (len(fruit))
+
+fruit = "APPLE"
+print (fruit.lower())
+
+fruit = "apple"
+print (fruit.upper())"""
+
+"""print ("red " + "hat")
+
+name = input ("whats your name? ")
+print (name)"""
+
+"""number1 == number2 #equels to
+number1 != number2 #not equel to
+number1 < number2 #is less than
+number1 > number2 #is more than
+number1 <= number2 #is less than or equel to
+number1 >= number2 #ia more than or equel to"""
+
+"""value1 = (20-10) >15
+value2 = (10+17) == 3**16
+value3 = 1**2 <= -1 
+value4 = 40* >= 
+value5 = 100 != 10*2
+
+value1 = False
+value2 = False
+value3 = False
+value4 = True
+value5 = False"""
+
+#and or not
+
+"""value1 = -(-(-(-2))) and (4 >= 16**0.5)
+value2 = 19%4 != 300/10/10 and false
+value3 = -(1**2)<2**0 and 10%10 <= 20-10*2
+
+value1 = False
+value2 = False
+value3 = True"""
+
+"""value1 = 2**3 == 108%100 or "Apple" == "orange"
+value2 = 100**0.5 >= 50 or False
+value3 = 1**100 == 100**1 or 3*2*1 != 3+2+1
+
+
+value1 = True
+value2 = False
+value3 = False"""
+
+"""value1 = not 3**4 < 4**3
+value2 = not 10%3 <= 10%2
+value3 = not not False
+
+value1 = True
+value2 = True
+value3 = False"""
+
+name = "bob"
+
+if 7 < 7:
+    print ("Hello")
+elif name == "bob":
+    print ("my name is bob")
+else:
+    print ("bye")
+    
+#and, or, not
+#use a comparator (==, >=, <=, >, <, !=)
+#print true
+
+value1 = not (67-45) >10
+value2 = 1**500 == 293**1 or
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
